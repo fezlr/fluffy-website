f9dd4966a5287c79406c93545146adfc455080ea
Artem <artem141789@gmail.com>
2026-03-24 19:16:22 +0300

    chore: initial commit

---
+++ src/main/java/fezlr/fluffy/FluffyApplication.java
@@ -0,0 +1,11 @@
+package fezlr.fluffy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluffyApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(FluffyApplication.class, args);
+	}
+}
---
+++ src/test/java/fezlr/fluffy/FluffyApplicationTests.java
@@ -0,0 +1,13 @@
+package fezlr.fluffy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluffyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
